858e7f426868f8b6f9315ab82690311462b0a52b
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-04-28 00:03:53 +0200

    Search is working

--- public/js/home.js
+++ public/js/home.js
@@ -4,6 +4,7 @@
     'use strict';
 
     var $search = $('#search');
+    var $searchTarget = $('#searchResults').hide();
 
     // Get all the articles
     $.get('/articles/')
@@ -44,5 +45,60 @@
 
     $search.keyup(function (e) {
         var search = $search.val();
+        var $articles = $('li .collection-item').toArray();
+
+        $searchTarget.slideUp(function () {
+            $searchTarget.empty();
+
+            $articles
+                // Extract article title
+                .map(function (e) {
+                    return [e.firstChild.nodeValue, $articles.indexOf(e)];
+                })
+                // Fuzzy match on the article title
+                .map(function (data) {
+                    var title = data[0];
+                    var index = data[1];
+
+                    var result = fuzzy.match(search, title, {
+                        pre: '<span class="highlight grey-text text-darken-2">',
+                        post: '</span>'
+                    });
+
+                    if (result) {
+                        result.index = index;
+                        return result;
+                    }
+                })
+                // Removes non-matching results
+                .filter(function (result) {
+                    return result !== undefined;
+                })
+                // Sort by score
+                .sort(function (a, b) {
+                    if (a.score > b.score) return 1;
+                    if (a.score < b.score) return -1;
+                    return 0;
+                })
+                // Generates HTML
+                .forEach(function (result) {
+                    $('<a/>')
+                        .attr('href', $articles[result.index].href)
+                        .addClass('collection-item grey-text text-darken-2')
+                        .html(result.rendered)
+                        .appendTo($searchTarget);
+                });
+
+            if (search.length > 0) {
+                if ($searchTarget.children().length > 0) {
+                    $search.removeClass('invalid').addClass('valid');
+                    $searchTarget.slideDown();
+                } else {
+                    $search.removeClass('valid').addClass('invalid');
+                }
+            } else {
+                $search.removeClass('invalid').removeClass('valid');
+            }
+        });
     });
 }());
