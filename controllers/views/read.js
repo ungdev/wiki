4ef11c31a00574a95ca26af21c09680db3388e34
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-04-29 15:57:01 +0200

    Zen reading Menu on read

--- controllers/views/read.js
+++ controllers/views/read.js
@@ -5,9 +5,9 @@
 module.exports = {
     method: 'get',
     route: '/read/:uid([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})',
-    controller: function (req, res, next) {
+    controller: function (req, res) {
         res.render('read', {
-            id: req.params.id
+            uid: req.params.uid
         });
     }
 };
--- public/js/read.js
+++ public/js/read.js
@@ -3,18 +3,25 @@
 (function () {
     'use strict';
 
+    var uid            = location.pathname.split('/')[2];
     var $articleTitle = $('#articleTitle');
     var $title        = $('title');
     var $target       = $('.target');
-    var article;
+    var $zen          = $('#zen');
+    var $editBtn      = $('.fixed-action-btn');
+    var $retypeName   = $('#retypeName');
+    var $delete       = $('.modal-footer button.disabled');
 
-    $.get('/articles/' + location.pathname.split('/')[2])
+    // Retrieve the article
+    $.get('/articles/' + uid)
         .done(function (res) {
-            article = res;
 
-            $title.append(' - ' + article.title);
-            $articleTitle.text(article.title);
-            var html = marked(article.content, {
+            $title.append(' - ' + res.title);
+            $articleTitle.text(res.title);
+            $retypeName.attr('placeholder', res.title);
+            $retypeName.next().addClass('active');
+
+            var html = marked(res.content, {
                 breaks: true,
                 sanitize: true,
                 highlight: function (code) {
@@ -27,7 +34,51 @@
             $('pre code').addClass('hljs');
         })
         .fail(function (res) {
-            location.href = '/error.html#' + res.status;
+            location.href = '/error/' + res.status;
         });
 
+    // Zen click
+    $zen.click(function () {
+        var isEnabled = !$zen.children().hasClass('mdi-action-visibility');
+
+        if (!isEnabled) {
+            $zen.css('opacity', 0.6);
+            $editBtn.fadeOut();
+            $zen.children().removeClass('mdi-action-visibility').addClass('mdi-action-visibility-off');
+            $('.read > .row:first-child').slideUp();
+            $('body').attr('style', 'background-color: #333;color: rgba(255, 255, 255, 0.6) !important');
+        } else {
+            $zen.css('opacity', 1);
+            $editBtn.fadeIn();
+            $zen.children().removeClass('mdi-action-visibility-off').addClass('mdi-action-visibility');
+            $('.read > .row:first-child').slideDown();
+            $('body').removeAttr('style');
+        }
+    });
+
+    // Remove article
+    $retypeName.keyup(function () {
+        if ($retypeName.val() === $retypeName.attr('placeholder')) {
+            $retypeName.removeClass('invalid').addClass('valid');
+            $delete.removeClass('disabled').removeAttr('disabled');
+        } else {
+            $retypeName.removeClass('valid').addClass('invalid');
+            $delete.addClass('disabled').attr('disabled', '');
+        }
+    });
+
+    $delete.click(function () {
+        $.ajax({ url: '/articles/' + uid, type: 'DELETE' })
+            .done(function () {
+                location.href = '/';
+            })
+            .fail(function (res) {
+                location.href = '/error/' + res.status;
+            })
+
+    });
+
+    // Enable zen tooltip and modals
+    $('.tooltipped').tooltip();
+    $('.modal-trigger').leanModal();
 }());
